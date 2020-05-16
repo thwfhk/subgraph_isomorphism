c84170348686adbdf49d5504ab55ce7b9a30f05a
wangchang <wangchang327@gmail.com>
2020-05-17 00:14:13 +0800

    fixed ullmann

--- vf2/driver_Ullman.cpp
+++ vf2/driver_Ullman.cpp
@@ -1,8 +1,8 @@
-#include "isomorphism.h"
 #include "../GraphDS.h"
-#include "Graph.h"
 #include "Graph.cpp"
+#include "Graph.h"
 #include "isomorphism.cpp"
+#include "isomorphism.h"
 
 // using namespace UGraph;
 
@@ -10,14 +10,14 @@ bool solve(Graph &P, Graph &G) {
     UGraph aa = UGraph(P.n);
     for (int i = 0; i < P.n; i++)
         for (int j = 0; j < P.n; j++)
-            if (P.g[i+1][j+1]) aa.insert(i, j);
-    for (int i = 0; i < P.n; i++) aa.label[i] = P.label[i+1];
+            if (P.g[i + 1][j + 1]) aa.insert(i, j);
+    for (int i = 0; i < P.n; i++) aa.label[i] = P.label[i + 1];
     UGraph bb = UGraph(G.n);
     for (int i = 0; i < G.n; i++)
         for (int j = 0; j < G.n; j++)
-            if (G.g[i+1][j+1]) bb.insert(i, j);
-    for (int i = 0; i < G.n; i++) bb.label[i] = G.label[i+1];
-    Ullmann u = Ullmann(aa, bb);
+            if (G.g[i + 1][j + 1]) bb.insert(i, j);
+    for (int i = 0; i < G.n; i++) bb.label[i] = G.label[i + 1];
+    Ullmann u = Ullmann(bb, aa);
     u.subisomorphism();
     return u.found;
 }
